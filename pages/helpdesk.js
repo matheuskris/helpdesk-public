dcc7c95c7d65dde0c7a2d5d87e93fed26a9fce7a
Miguel <miguel2014ssa@gmail.com>
2022-10-26 15:57:57 -0300

    finish table template

--- pages/helpdesk.js
+++ pages/helpdesk.js
@@ -4,6 +4,77 @@ import { useRouter } from "next/router";
 function Helpdesk() {
   const router = useRouter();
 
+  // just an example of what the date should look like
+  function data() {
+    return `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()} às ${new Date().getHours()}:${new Date().getMinutes()}`
+  }
+
+  const chamados = [
+    {
+      numeroChamado: 1,
+      dataInicial: data(),
+      DataTermino: data(),
+      Título: 'Computador lento',
+      Descrição: 'Lento demais',
+      Prioridade: 'A',
+      Responsável: 'Patricia',
+    },
+    {
+      numeroChamado: 2,
+      dataInicial: data(),
+      DataTermino: data(),
+      Título: 'Computador não liga',
+      Descrição: 'não liga',
+      Prioridade: 'C',
+      Responsável: 'Flávio',
+    },
+    {
+      numeroChamado: 3,
+      dataInicial: data(),
+      DataTermino: data(),
+      Título: 'Não consigo cadastrar compra',
+      Descrição: 'ERP deu pau',
+      Prioridade: 'B',
+      Responsável: 'Monica',
+    },
+    {
+      numeroChamado: 4,
+      dataInicial: data(),
+      DataTermino: data(),
+      Título: 'internet caiu',
+      Descrição: 'não fui eu',
+      Prioridade: 'A',
+      Responsável: 'Patricia',
+    },
+    {
+      numeroChamado: 5,
+      dataInicial: data(),
+      DataTermino: data(),
+      Título: 'internet caiu de novo',
+      Descrição: 'não fui eu',
+      Prioridade: 'C',
+      Responsável: 'Monica',
+    },
+    {
+      numeroChamado: 6,
+      dataInicial: data(),
+      DataTermino: data(),
+      Título: 'Reposição de teclado',
+      Descrição: 'Teclado quebrou',
+      Prioridade: 'B',
+      Responsável: 'Flávio',
+    },
+    {
+      numeroChamado: 7,
+      dataInicial: data(),
+      DataTermino: data(),
+      Título: 'Reposição de teclado',
+      Descrição: 'Teclado quebrou',
+      Prioridade: 'B',
+      Responsável: 'Flávio',
+    },
+  ]
+
   function logout() {
     router.push("/");
   }
@@ -13,7 +84,7 @@ function Helpdesk() {
   }
 
   return (
-    <div className="h-screen w-full relative">
+    <div className="h-screen w-full relative bg-[#FFF]">
 
       {/* Aside */}
       <div className="lg:absolute lg:w-[136px] lg:h-full bg-[#c4c4c4]">
@@ -75,23 +146,42 @@ function Helpdesk() {
 
       {/* Titulo tabela e botão  */}
       <div className='flex justify-between items-center'>
-        <h1 className=" text-lg font-semibold ml-[136px] pl-16 mb-2">Painel de Controle</h1>
+        <h1 className=" text-3xl font-semibold ml-[136px] pl-16 mb-2">Painel de Controle</h1>
         <button onClick={criarChamado} className="btnAddChamado">Adicionar um chamado</button>
       </div>
 
       {/* Tabela */}
       <div className="w-[calc(100% - 136px)] h-[70%] mx-auto flex justify-center items-center ml-[136px]">
-        <table className="w-[100%] h-[100%] bg-red-400 px-10 py-12 rounded-3xl">    
+        <table className="w-[100%] h-[100%] bg-white rounded-3xl border-collapse mx-[60px] my-0 text-lg ">    
             <thead>
-              <tr>
-                <th>Número do Chamado</th>
-                <th>Título</th>
-                <th>Descrição</th>
-                <th>Prioridade</th>
-                <th>Responsável</th>
+              <tr className="bg-[#c4c4c4] text-white text-left">
+                <th className="th">ID Chamado</th>
+                <th className="th">Data Inicial</th>
+                <th className="th">Data Término</th>
+                <th className="th">Título</th>
+                <th className="th">Descrição</th>
+                <th className="th">Prioridade</th>
+                <th className="th">Responsável</th>
               </tr>
             </thead>
+            <tbody> 
+              {chamados.map((chamado) => (
+                <tr key={chamado.id} className='border-b border-[#dddddd] even:bg-gray-200'>
+                  <td className="td">{chamado.numeroChamado}</td>
+                  <td className="td">{chamado.dataInicial}</td>
+                  <td className="td">{chamado.DataTermino}</td>
+                  <td className="td">{chamado.Título}</td>
+                  <td className="td">{chamado.Descrição}</td>
+                  <td className="td">{chamado.Prioridade}</td>
+                  <td className="td">{chamado.Responsável}</td>
+                </tr>
+              ))}
+            </tbody>
         </table>
+      </div>
+
+      <div className="w-full mx-auto">
+        <p className="font-bold">1, 2, 3...</p>
       </div>
 
     </div>
