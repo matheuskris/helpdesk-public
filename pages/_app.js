914bc907b5d7a5a54a0e5d471168f710a4d7f164
Miguel <miguel2014ssa@gmail.com>
2022-10-26 10:04:07 -0300

    cleaning project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <div>
+        <Head>
+          <title>Help | Desk</title>
+          <meta name="description" content="" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </div>
+    )
 }
 
-export default MyApp
+export default MyApp;
