1da945866bdf4b937d4b3012dec6a50871922fbd
Miguel <miguel2014ssa@gmail.com>
2022-10-27 16:02:44 -0300

    Modal and Select Priority done

--- pages/helpdesk.js
+++ pages/helpdesk.js
@@ -2,10 +2,78 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { getObjectsOfCollection } from '../src/utils/firebase.utils';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#__next')
 
 function Helpdesk() {
   const router = useRouter();
+  const [infoCall, setInfoCall] = useState({
+    id: "",
+    start: "",
+    title: "",
+    description: "",
+    priority: "",
+    inCharge: "",
+  });
+  const [isRegisterFull, setIsRegisterFull] = useState(true);
   const [chamados, setChamados] = useState([]);
+  const [modalIsOpen, setModal] = useState(false)
+
+  // ====== Funções do Modal =========
+  function handleOpenModal() {
+    setModal(true);
+  }
+  function handleCloseModal() {
+    setModal(false)
+  }
+  // Estilo do Modal
+  const customStyle = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      borderRadius: '24px',
+    }
+  }
+  // adding new call to firebase thru the modal
+  async function handleRegister(e) {
+    e.preventDefault();
+    data = new Date();
+    const objectToSend = { ...infoCall, start:`${data.getDate()}/${
+      data.getMonth() + 1
+    }/${data.getFullYear()} às ${data.getHours()}:${data.getMinutes()}` };
+
+    console.log(objectToSend)
+    for (const prop in objectToSend) {
+      if (!objectToSend[prop]) {
+        setIsRegisterFull(false);
+        return;
+      }
+    }
+
+    await addObjectToCollection("calls", objectToSend);
+    setInfoCall({
+      id: "",
+      start: "",
+      title: "",
+      description: "",
+      priority: "",
+      inCharge: "",
+    });
+    window.location.reload();
+  }
+
+
+  // Normal handle change
+  function handleChange(e) {
+    const { value } = e.target;
+    const { name } = e.target;
+    setInfoCall({ ...infoCall, [name]: value });
+  }
 
   // checking if the user is authenticated if not, pushing to login page
   useEffect(() => {
@@ -23,6 +91,7 @@ function Helpdesk() {
 
   }, [])
 
+  // Test data
   useEffect(()=> {
     const datahoje =  new Date()
     console.log(datahoje.toDateString())
@@ -40,11 +109,6 @@ function Helpdesk() {
   function logout() {
     localStorage.removeItem("user");
     router.push("/");
-  }
-
-  //Button Create Call
-  function goToPageCreateCall() {
-    router.push("criarchamado");
   }
 
   return (
@@ -105,9 +169,10 @@ function Helpdesk() {
         <h1 className=" text-3xl font-semibold ml-[136px] pl-16 mb-2">
           Painel de Controle
         </h1>
-        <button onClick={goToPageCreateCall} className="btnAddChamado">
+        <button onClick={handleOpenModal} className="btnAddChamado">
           Abrir um chamado
         </button>
+        
       </div>
 
       {/* Tabela */}
@@ -126,7 +191,7 @@ function Helpdesk() {
               <th className="th"></th>
             </tr>
           </thead>
-          <tbody className="overflow-y-scroll">
+          <tbody>
             {chamados.map((chamado) => (
               <tr
                 key={chamado.id}
@@ -140,12 +205,78 @@ function Helpdesk() {
                 <td className="td">{chamado.priority}</td>
                 <td className="td">{chamado.inCharge}</td>
                 <td className="td"><button className='p-2 rounded-lg font-semibold cursor-pointer bg-yellow-300'>Editar</button></td>
-                <td className="td"><button className="p-2 rounded-lg font-semibold cursor-pointer bg-red-600">Finalizar</button></td>
+                <td className="td"><button className="p-2 rounded-lg font-semibold cursor-pointer bg-red-500">Finalizar</button></td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+
+
+      {/* === Configuração do Modal =====  */}
+      <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={handleCloseModal}
+          style={customStyle}
+        >
+          <form
+        onSubmit={handleRegister}
+        className="w-[100%] h-[100%] mx-auto flex flex-col"
+      >
+        <h1 className="text-black text-2xl mx-auto mt-10 mb-8 border-b border-gray-400">
+          Detalhes do Chamado
+        </h1>
+        <input
+          onChange={handleChange}
+          name="id"
+          value={infoCall.id}
+          placeholder="ID"
+          className="inputCadastro"
+          type="number"
+        />
+        <input
+          onChange={handleChange}
+          name="title"
+          value={infoCall.title}
+          placeholder="Título"
+          className="inputCadastro"
+          type="text"
+        />
+        <input
+          onChange={handleChange}
+          name="description"
+          value={infoCall.description}
+          placeholder="Descrição"
+          className="inputCadastro"
+          type="text"
+        />
+        <select
+          onChange={handleChange}
+          name="priority"
+          value={infoCall.priority}
+          className="p-4 outline-none inputCadastro"
+        >
+          <option defaultValue={true}>Prioridade:</option>
+          <option value="Alta">Alta</option>
+          <option value="Média">Média</option>
+          <option value="Baixa">Baixa</option>
+        </select>
+        <select
+          onChange={handleChange}
+          name="inCharge"
+          value={infoCall.inCharge}
+          className="p-4 outline-none inputCadastro"
+        >
+          <option defaultValue={true}>Responsável:</option>
+          <option value="Flávio">Flávio</option>
+          <option value="Patrícia">Patrícia</option>
+          <option value="Mônica">Mônica</option>
+        </select>
+        <button className="btnCadastrar">Cadastrar</button>
+        </form>
+      </Modal>
+
+      
     </div>
   );
 }
