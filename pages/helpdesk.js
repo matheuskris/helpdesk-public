461d7755bd10d3b72a8a4f2846905ab4c9a8d7e4
Matheus Krisciumas <matheus.kris@gmail.com>
2022-10-28 14:21:17 -0300

    editMode and filtered

--- pages/helpdesk.js
+++ pages/helpdesk.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { callsListener } from "../src/utils/firebase.utils";
+import { callsListener, writeNewCall } from "../src/utils/firebase.utils";
 import CreateCallModal from "../src/components/CreateCallModal";
 import EditCallModal from "../src/components/EditCallModal";
 import DescriptionModal from "../src/components/DescriptionModal";
@@ -9,16 +9,56 @@ import DescriptionModal from "../src/components/DescriptionModal";
 function Helpdesk() {
   const router = useRouter();
   const [chamados, setChamados] = useState([]);
-  const [isModalOpen, setModal] = useState(false);
-  const [isEditModalOpen, setEditModal] = useState(false);
-  const [isDescriptionModalOpen, setDescriptionModal] = useState(false);
-  const [closedCalls, setClosedCalls] = useState([]);
-  const [description, setDescription] = useState("");
 
-  // open modal
-  function handleOpenModal() {
-    setModal(true);
-  }
+  const [isModalOpen, setModal] = useState(false);
+
+  const [isEditModalOpen, setEditModal] = useState(false);
+  const [callToEdit, setCallToEdit] = useState();
+
+  const [isDescriptionModalOpen, setDescriptionModal] = useState(false);
+  const [description, setDescription] = useState("");
+  const [filterOrderBy, setFilterOrderBy] = useState("priority");
+
+  const filteredCalls = chamados.sort(function (a, b) {
+    switch (filterOrderBy) {
+      case "start":
+        return a.start.hour - b.start.hour;
+      case "id":
+        return a.id - b.id;
+      case "inCharge":
+        let xIC = a.title.toLowerCase();
+        let yIC = b.title.toLowerCase();
+        if (xIC < yIC) {
+          return -1;
+        }
+        if (xt > yt) {
+          return 1;
+        }
+        return 0;
+      case "title":
+        let xt = a.title.toLowerCase();
+        let yt = b.title.toLowerCase();
+        if (xt < yt) {
+          return -1;
+        }
+        if (xt > yt) {
+          return 1;
+        }
+        return 0;
+      case "priority":
+        let x = a.priority.toLowerCase();
+        let y = b.priority.toLowerCase();
+        if (x < y) {
+          return -1;
+        }
+        if (x > y) {
+          return 1;
+        }
+        return 0;
+    }
+  });
+
+  console.log(filteredCalls);
 
   // checking if the user is authenticated if not, pushing to login page
   useEffect(() => {
@@ -41,10 +81,6 @@ function Helpdesk() {
     callsListener(transformObjectToArray);
   }, []);
 
-  // useEffect(() => {
-  //   getObjectsOfCollection("calls").then((calls) => setChamados(calls));
-  // }, []);
-
   // Logaut logic
   function logout() {
     localStorage.removeItem("user");
@@ -52,15 +88,35 @@ function Helpdesk() {
   }
 
   // handleCloseCall
-  function handleCloseCall(id) {
-    const chosenCall = chamados.filter((chamado) => chamado.id === id);
-    setClosedCalls([...closedCalls, chosenCall]);
+  function getBeatyDate() {
+    const date = new Date();
+
+    return {
+      day: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
+      hour: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+    };
+  }
+
+  function handleCloseCall(callToClose) {
+    const newDate = getBeatyDate();
+
+    writeNewCall({ ...callToClose, finished: newDate });
+  }
+
+  // open modal
+  function handleOpenModal() {
+    setModal(true);
   }
 
   // opening the modal and setting the description to get in the modal
   function checkDescription(desc) {
     setDescriptionModal(true);
     setDescription(desc);
+  }
+
+  function handleEditModal(chamado) {
+    setEditModal(true);
+    setCallToEdit(chamado);
   }
 
   return (
@@ -143,7 +199,7 @@ function Helpdesk() {
               </tr>
             </thead>
             <tbody>
-              {chamados.map((chamado) => (
+              {filteredCalls.map((chamado) => (
                 <tr
                   key={chamado.id}
                   className="border-b border-[#dddddd] even:bg-gray-200 mb-4"
@@ -162,7 +218,7 @@ function Helpdesk() {
                   <td className="td">{chamado.inCharge}</td>
                   <td className="td">
                     <button
-                      onClick={() => setEditModal(true)}
+                      onClick={() => handleEditModal(chamado)}
                       className="btnEdit"
                     >
                       Editar
@@ -170,7 +226,7 @@ function Helpdesk() {
                   </td>
                   <td className="td">
                     <button
-                      onClick={() => handleCloseCall(chamado.id)}
+                      onClick={() => handleCloseCall(chamado)}
                       className="btnCloseCall"
                     >
                       Finalizar
@@ -199,6 +255,8 @@ function Helpdesk() {
       <EditCallModal
         isEditModalOpen={isEditModalOpen}
         setEditModal={setEditModal}
+        callToEdit={callToEdit}
+        setCallToEdit={setCallToEdit}
       />
       <DescriptionModal
         description={description}
