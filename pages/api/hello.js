cbc722a11c17a0a45fcfda47be0cba65a737020e
Matheus Krisciumas <matheus.kris@gmail.com>
2022-12-20 13:11:31 -0300

    hi

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,26 +5,34 @@ import { sendInviteToToProject } from "../../src/utils/firebase.utils";
 import app from "../../src/utils/firebase-admin.utils";
 
 export default async function handler(req, res) {
-  try {
-    const { method } = req;
-    switch (method) {
-      case "POST":
-        const { type } = req.body.body;
-        if (type === "sendInvite") {
-          const { projectUid, name, email, projectName } = req.body.body;
+  const { method } = req;
+  let responseObject = {};
+  switch (method) {
+    case "POST":
+      const { type } = req.body.body;
+      if (type === "sendInvite") {
+        const { projectUid, name, email, projectName } = req.body.body;
 
-          console.log("vamos tentar encontrar o user");
+        console.log("vamos tentar encontrar o user");
+        responseObject.start = "vamos tentar encontrar o user";
 
+        try {
           const user = await getAuth(app)
             .getUserByEmail(email)
             .then((userRec) => userRec.toJSON())
             .catch((err) => {
+              responseObject.notFound = "não conseguimos encontrar o usuario";
               console.log("não conseguimos encontrar o usuario");
               res.status(404).json({
                 message: err.code,
+                responseObject,
               });
             });
           if (user) {
+            responseObject.found = {
+              text: "achamos um user, vamos tentar mandar o convite para:",
+              userUid: user.uid,
+            };
             console.log(
               "achamos um user, vamos tentar mandar o convite para:",
               user.uid
@@ -38,52 +46,62 @@ export default async function handler(req, res) {
             res.status(200).json({
               statusCode: 200,
               message: "succesfully send your invite",
+              responseObject,
             });
           }
-        }
-        if (type === "acceptInvite") {
-          const { projectUid, name, uid } = req.body.body;
-
-          console.log("accept start", projectUid);
-
-          let projectInfo;
-          await getDatabase(app)
-            .ref("/projects/" + projectUid)
-            .once("value", (snapshot) => {
-              projectInfo = snapshot.val();
-            });
-
-          const projectInfoToSend = {
-            createdAt: projectInfo.createdAt,
-            createdBy: projectInfo.createdBy,
-            key: projectInfo.key,
-            name: projectInfo.name,
-          };
-
-          console.log(projectInfoToSend);
-
-          const db = getDatabase(app);
-          const usersRef = db.ref("/projects/" + projectUid + "/users/");
-          const userRef = usersRef.child(uid);
-          await userRef.set(name);
-
-          const userProjectsRef = db.ref("/users/" + uid + "/projects/");
-          const projectRef = userProjectsRef.child(projectInfo.key);
-          await projectRef.set(projectInfoToSend);
-
-          const invitesRef = db.ref("/users/" + uid + "/invites/");
-          const inviteRef = invitesRef.child(projectInfo.key);
-          await inviteRef.set(null);
-
-          res.status(200).json({
-            message: "you successfully accepted your invite!",
+        } catch (err) {
+          res.status(500).json({
+            erro: err,
+            statusCode: 500,
+            message: "there was some error",
+            responseObject,
           });
         }
-        break;
-      default:
-        res.status(405).end(`Method ${method} Not Allowed`);
-    }
-  } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message });
+      }
+      if (type === "acceptInvite") {
+        const { projectUid, name, uid } = req.body.body;
+
+        console.log("accept start", projectUid);
+
+        let projectInfo;
+        await getDatabase(app)
+          .ref("/projects/" + projectUid)
+          .once("value", (snapshot) => {
+            projectInfo = snapshot.val();
+          });
+
+        const projectInfoToSend = {
+          createdAt: projectInfo.createdAt,
+          createdBy: projectInfo.createdBy,
+          key: projectInfo.key,
+          name: projectInfo.name,
+        };
+
+        console.log(projectInfoToSend);
+
+        const db = getDatabase(app);
+        const usersRef = db.ref("/projects/" + projectUid + "/users/");
+        const userRef = usersRef.child(uid);
+        await userRef.set(name);
+
+        const userProjectsRef = db.ref("/users/" + uid + "/projects/");
+        const projectRef = userProjectsRef.child(projectInfo.key);
+        await projectRef.set(projectInfoToSend);
+
+        const invitesRef = db.ref("/users/" + uid + "/invites/");
+        const inviteRef = invitesRef.child(projectInfo.key);
+        await inviteRef.set(null);
+
+        res.status(200).json({
+          message: "you successfully accepted your invite!",
+        });
+      }
+      break;
+    default:
+      res.status(405).end(`Method ${method} Not Allowed`);
   }
+
+  // } catch (err) {
+  //   res.status(500).json({ statusCode: 500, message: err.message });
+  // }
 }
