929256cf4b2fbb4793c8ae59e35b9fb944273ea6
Matheus Krisciumas <matheus.kris@gmail.com>
2022-12-21 10:48:35 -0300

    trying to fix server 4.0.2

--- src/utils/firebase-admin.utils.js
+++ src/utils/firebase-admin.utils.js
@@ -1,13 +1,21 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 
-const privateKeyId =
-  process.env.GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID.replace(
-    /\\n/g,
-    "\n"
-  );
+// const privateKeyId =
+//   process.env.GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID.replace(
+//     /\\n/g,
+//     "\n"
+//   );
 
-const privateKey =
-  process.env.GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY.replace(/\\n/g, "\n");
+const privateKeyId = JSON.parse(
+  process.env.GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID
+);
+
+// const privateKey =
+//   process.env.GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY.replace(/\\n/g, "\n")
+
+const privateKey = JSON.parse(
+  process.env.GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY
+);
 
 const firebaseCertConfig = {
   type: "service_account",
